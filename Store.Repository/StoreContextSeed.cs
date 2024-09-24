f6e623ab7b5fd863ecd676f31ae28f526c420d01
esraa <esraa@DESKTOP-9RKGVE6>
2024-09-24 15:58:39 +0300

    Apply Seeding

--- Store.Repository/StoreContextSeed.cs
+++ Store.Repository/StoreContextSeed.cs
@@ -13,40 +13,45 @@ namespace Store.Repository
 {
     public class StoreContextSeed
     {
-        public static async Task SeedAsync(StoreDbContext context , ILoggerFactory loggerFactory)
+        public static async Task SeedAsync(StoreDbContext context, ILoggerFactory loggerFactory)
         {
             try
             {
-                if(context.ProductBrands != null && context.ProductBrands.Any())
+                if (context.ProductBrands != null && !context.ProductBrands.Any())
                 {
-                    //Presist Data to DB
-                    var brandsData = File.ReadAllText("../Store.Repoistory/SeedData/brands.json");
+                    var brandsData = File.ReadAllText("../Store.Repository/SeedData/brands.json");
                     var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+
                     if (brands is not null)
                         await context.ProductBrands.AddRangeAsync(brands);
                 }
-                if (context.ProductTypes != null && context.ProductTypes.Any())
+
+                if (context.ProductTypes != null && !context.ProductTypes.Any())
                 {
-                    //Presist Data to DB
-                    var typesData = File.ReadAllText("../Store.Repoistory/SeedData/types.json");
+                    var typesData = File.ReadAllText("../Store.Repository/SeedData/types.json");
                     var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+
                     if (types is not null)
                         await context.ProductTypes.AddRangeAsync(types);
                 }
-                if (context.Products != null && context.Products.Any())
+
+                if (context.Products != null && !context.Products.Any())
                 {
-                    //Presist Data to DB
-                    var productsData = File.ReadAllText("../Store.Repoistory/SeedData/product.json");
+                    var productsData = File.ReadAllText("../Store.Repository/SeedData/products.json");
                     var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
                     if (products is not null)
                         await context.Products.AddRangeAsync(products);
                 }
+
+                await context.SaveChangesAsync();
             }
             catch (Exception ex)
             {
-                    var logger = loggerFactory.CreateLogger<StoreContextSeed>();
-                    logger.LogError(ex.Message);
+                var logger = loggerFactory.CreateLogger<StoreContextSeed>();
+                logger.LogError(ex.Message);
             }
         }
     }
-}
+    }
+
