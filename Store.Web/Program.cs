926d096110a6a48bcecbfad3ae0afbcfb55e4e8b
esraa <esraa@DESKTOP-9RKGVE6>
2024-10-01 11:43:13 +0300

    Refactoring Program Class

--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -8,6 +8,7 @@ using Store.Repository.Repositories;
 using Store.Service.HandleResponses;
 using Store.Service.Services.ProductServices;
 using Store.Service.Services.ProductServices.Dtos;
+using Store.Web.Extensions;
 using Store.Web.Helper;
 using Store.Web.Middleware;
 
@@ -27,26 +28,7 @@ namespace Store.Web
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefultConnection"));
             });
 
-            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-            builder.Services.AddScoped<IProductService, ProductService>();
-            builder.Services.AddAutoMapper(typeof(ProductProfile));
-
-            builder.Services.Configure<ApiBehaviorOptions>(options
-                =>
-            {
-                options.InvalidModelStateResponseFactory = actionContext =>
-                {
-                    var errors = actionContext.ModelState.Where(model => model.Value?.Errors.Count > 0)
-                    .SelectMany(model => model.Value?.Errors)
-                    .Select(error => error.ErrorMessage)
-                    .ToList();
-                    var errorResponse = new ValidationErrorResponse
-                    {
-                        Errors = errors
-                    };
-                    return new BadRequestObjectResult(errorResponse);
-                };
-            });
+            builder.Services.AddApplicationServices();
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
