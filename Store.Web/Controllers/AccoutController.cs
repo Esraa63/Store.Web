fb22c560ccd7711b724d771628cbebb63edd28f6
esraa <esraa@DESKTOP-9RKGVE6>
2024-10-09 09:35:14 +0300

    UserService [Login] - Full Cycle

---
+++ Store.Web/Controllers/AccoutController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Store.Service.HandleResponses;
+using Store.Service.Services.UserService;
+using Store.Service.Services.UserService.Dtos;
+
+namespace Store.Web.Controllers
+{
+    
+    public class AccoutController : BaseController
+    {
+        private readonly IUserService _userService;
+
+        public AccoutController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost]
+        public async Task<ActionResult<UserDto>> Login(LoginDto input)
+        {
+            var user = await _userService.Login(input);
+            if (user is null)
+                return BadRequest(new CustomException(400,"Email Dose Not Exists"));
+            return Ok(user);
+        }
+    }
+}
--- Store.Web/Extensions/ApplicationServiceExtension.cs
+++ Store.Web/Extensions/ApplicationServiceExtension.cs
@@ -9,6 +9,7 @@ using Store.Service.Services.BasketService.Dto;
 using Store.Service.Services.BasketService;
 using Store.Repository.Basket;
 using Store.Service.Services.TokenService;
+using Store.Service.Services.UserService;
 
 
 namespace Store.Web.Extensions
@@ -23,6 +24,7 @@ namespace Store.Web.Extensions
             services.AddScoped<IBasketRepoistory, BasketRepoistory>();
             services.AddScoped<IBasketService, BasketService>();
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserService, UserService>();
             services.AddAutoMapper(typeof(ProductProfile));
             services.AddAutoMapper(typeof(BasketProfile));
             services.Configure<ApiBehaviorOptions>(options
