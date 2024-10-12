857dd4d512d235226a320aac4122f5a9dcacd062
esraa <esraa@DESKTOP-9RKGVE6>
2024-10-12 16:19:59 +0300

    OrderService [CreateOrderAsync]

--- Store.Service/OrderService/OrderService.cs
+++ Store.Service/OrderService/OrderService.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using AutoMapper;
+using Store.Data.Entities;
+using Store.Data.Entities.OrderEntities;
+using Store.Repository.Interfaces;
+using Store.Service.OrderService.Dtos;
+using Store.Service.Services.BasketService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +14,99 @@ namespace Store.Service.OrderService
 {
     public class OrderService : IOrderService
     {
+        private readonly IBasketService _basketService;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
+        public OrderService(IBasketService basketService
+            , IUnitOfWork unitOfWork,
+            IMapper mapper)
+        {
+            _basketService = basketService;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task<OrderDetailsDto> CreateOrderAsync(OrderDto input)
+        {
+            var basket=await _basketService.GetBasketAsync(input.BasketId);
+            if (basket == null)
+                throw new Exception("Basket Not Exist");
+            #region Fill Order Item List With Items in the Basket
+
+            var orderItems = new List<OrderItemDto>();
+            foreach (var item in basket.BascetItems)
+            {
+                var productItem = await _unitOfWork.Repoistory<Product, int>().GetByIdAsync(item.ProductId);
+                if (productItem == null)
+                    throw new Exception($"Product With Id : {item.ProductId} Not Exist");
+                var itemOrdered = new ProductItem
+                {
+                    PictureUrl = productItem.PictureUrl,
+                    ProductName = productItem.Name,
+                    ProductId = item.ProductId
+                };
+                var orderItem = new OrderItem
+                {
+                    Price = productItem.Price,
+                    Quantity = item.Quantity,
+                    ProductItem = itemOrdered
+                };
+                var mappedOrderItem = _mapper.Map<OrderItemDto>(orderItem);
+                orderItems.Add(mappedOrderItem);
+            }
+                #endregion
+
+                #region Get DeliveryMethod
+
+                var deliveryMethod = await _unitOfWork.Repoistory<DeliveryMethod, int>().GetByIdAsync(input.DeliveryMethodId);
+                if (deliveryMethod == null)
+                    throw new Exception("DeliveryMethod Not Provided");
+                #endregion
+
+
+                #region Caluclate Subtotal
+
+                var subtotal = orderItems.Sum(item => item.Quantity * item.Price);
+                #endregion
+
+                #region To Do Payment
+
+                #endregion
+
+                #region Create Order
+                var mappedShippingAddress = _mapper.Map<ShippingAddress>(input.ShippingAddress);
+                var mappedOrderItems=_mapper.Map<List<OrderItem>>(orderItems);
+                var order = new Order
+                {
+
+                    DeliveryMethodId = deliveryMethod.Id,
+                    ShippingAddress = mappedShippingAddress,
+                    BuyerEmail= input.BuyerEmail,
+                    BasketId= input.BasketId,
+                    OrderItems=mappedOrderItems,
+                    SubTotal=subtotal,
+                };
+                await _unitOfWork.Repoistory<Order, Guid>().AddAsync(order);
+                await _unitOfWork.CompleteAsync();
+                var mappedOrder = _mapper.Map<OrderDetailsDto>(order);
+                return mappedOrder;
+                #endregion
+            
+        }
+
+        public Task<IReadOnlyList<DeliveryMethod>> GetAllDeliveryMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<OrderDetailsDto>> GetAllOrdersForUserAsync(string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderDetailsDto> GetOrderByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
