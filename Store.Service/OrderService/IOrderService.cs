e975444eb73558d3a1c03036723336ad160c79f2
esraa <esraa@DESKTOP-9RKGVE6>
2024-10-12 15:13:52 +0300

    OrderService [OrderProfile] For Mapper

---
+++ Store.Service/OrderService/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using Store.Data.Entities;
+using Store.Service.OrderService.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Service.OrderService
+{
+    public interface IOrderService
+    {
+        Task<OrderDetailsDto> CreateOrderAsync(OrderDto orderDto);
+        Task<IReadOnlyList<OrderDetailsDto>> GetAllOrdersForUserAsync(string buyerEmail);
+        Task<OrderDetailsDto> GetOrderByIdAsync(Guid id);
+        Task<IReadOnlyList<DeliveryMethod>> GetAllDeliveryMethod();
+    }
+}
